1ac34800888ac09d0c27329be6b7f9a3690e1d26
Piotr Borek <piotrborek@op.pl>
2025-04-02 21:26:52 +0200

    Added missing functions to trait.

--- oghma/src/main/scala/dev/pb/oghma/api/Marshal.scala
+++ oghma/src/main/scala/dev/pb/oghma/api/Marshal.scala
@@ -5,3 +5,5 @@ import fs2.Stream
 trait Marshal[F[_]]:
   def marshalLong(value: Long): Stream[F, Byte]
   def marshalInt(value: Int): Stream[F, Byte] = marshalLong(value)
+  def marshalShort(value: Short): Stream[F, Byte] = marshalLong(value)
+  def marshalByte(value: Byte): Stream[F, Byte] = marshalLong(value)
