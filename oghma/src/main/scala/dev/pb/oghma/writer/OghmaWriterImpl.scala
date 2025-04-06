62bf46a59fbfbf40c0cd8b423e18b07ab40a621e
Piotr Borek <piotrborek@op.pl>
2025-04-06 08:47:46 +0200

    Small changes. Tests passed.

--- oghma/src/main/scala/dev/pb/oghma/writer/OghmaWriterImpl.scala
+++ oghma/src/main/scala/dev/pb/oghma/writer/OghmaWriterImpl.scala
@@ -6,12 +6,14 @@ trait OghmaWriterImpl extends OghmaWriter:
   this: ByteWriter =>
 
   def writeLong(value: Long): this.type =
-    value match
-      case x if x >= -128 && x <= 127                 => WriteInt.writeInt8(x).collect(this)
-      case x if x >= -32_768 && x <= 32_767           => WriteInt.writeInt16(x).collect(this)
-      case x if x >= -2147483648L && x <= 2147483647L => WriteInt.writeInt32(x).collect(this)
-      case x                                          => WriteInt.writeInt64(x).collect(this)
+    val buffer =
+      value match
+        case x if x >= -128 && x <= 127                 => WriteInt.writeInt8(x)
+        case x if x >= -32_768 && x <= 32_767           => WriteInt.writeInt16(x)
+        case x if x >= -2147483648L && x <= 2147483647L => WriteInt.writeInt32(x)
+        case x                                          => WriteInt.writeInt64(x)
 
+    buffer.collect(this)
     this
 
   def writeFloat32(value: Float): this.type =
@@ -23,8 +25,11 @@ trait OghmaWriterImpl extends OghmaWriter:
     this
 
   def writeBinary(value: Array[Byte]): this.type =
-    value match
-      case x if x.length <= 255    => WriteBinary.writeSmallBinary(value).collect(this)
-      case x if x.length <= 65_535 => WriteBinary.writeMediumBinary(value).collect(this)
-      case x                       => WriteBinary.writeLargeBinary(value).collect(this)
+    val buffer =
+      value match
+        case x if x.length <= 255    => WriteBinary.writeSmallBinary(x)
+        case x if x.length <= 65_535 => WriteBinary.writeMediumBinary(x)
+        case x                       => WriteBinary.writeLargeBinary(x)
+
+    buffer.collect(this)
     this
--- oghma/src/main/scala/dev/pb/oghma/writer/WriteFloat.scala
+++ oghma/src/main/scala/dev/pb/oghma/writer/WriteFloat.scala
@@ -3,13 +3,13 @@ package dev.pb.oghma.writer
 import dev.pb.oghma.common.ByteBuffer
 
 private[writer] object WriteFloat:
-  def writeFloat32(value: Float) =
+  def writeFloat32(value: Float): ByteBuffer#Opts =
     ByteBuffer
       .allocBigEndian(4)
       .putFloat(value)
       .complete
 
-  def writeFloat64(value: Double) =
+  def writeFloat64(value: Double): ByteBuffer#Opts =
     ByteBuffer
       .allocBigEndian(8)
       .putDouble(value)
