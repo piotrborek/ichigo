ccdaa54eb555737e63e4922a34c71f50014a5650
Piotr Borek <piotrborek@op.pl>
2025-04-06 21:24:44 +0200

    Fixed test.

--- oghma/src/test/scala/dev/pb/oghma/writer/OghmaWriterTest.scala
+++ oghma/src/test/scala/dev/pb/oghma/writer/OghmaWriterTest.scala
@@ -85,7 +85,7 @@ class OghmaWriterTest extends FunSuite:
     assertEquals(obtained, expected)
 
   test("writeDateTime should write data - test 1"):
-    val dateTime = LocalDateTime.parse("2025-04-06T21:04:27.22870542")
+    val dateTime = LocalDateTime.parse("2025-04-06T21:04:27.022870542")
     val expected = Vector(0x20, 0x25, 0xf4, 0x06, 0x21, 0x04, 0x27, 0x22, 0x87, 0x05, 0x42, 0xf0).map(_.toByte)
     val obtained =
       MemoryOghmaWriter()
